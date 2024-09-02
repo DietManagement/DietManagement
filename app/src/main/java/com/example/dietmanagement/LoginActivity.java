803a1f3c5a1525577f96208a99447fff287ebf69
WeePoong <080202jungwoo@gmail.com>
2024-09-02 18:18:10 +0900

    거의 다함

--- app/src/main/java/com/example/dietmanagement/LoginActivity.java
+++ app/src/main/java/com/example/dietmanagement/LoginActivity.java
@@ -1,6 +1,9 @@
 package com.example.dietmanagement;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,13 +11,33 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class ProfileActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity {
+
+    EditText ID = findViewById(R.id.idET_login);
+    EditText PW = findViewById(R.id.pwET_login);
+    Button login = findViewById(R.id.loginBTN_login);
+    Button signup = findViewById(R.id.singUpBTN_login);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_profile);
+        setContentView(R.layout.activity_login);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
---
+++ app/src/main/java/com/example/dietmanagement/SingUpActivity.java
@@ -0,0 +1,61 @@
+package com.example.dietmanagement;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class SingUpActivity extends AppCompatActivity {
+
+    EditText id = findViewById(R.id.idET_singUp);
+    Button duplication_id = findViewById(R.id.idDoubleCheckBTN_singUp);
+    EditText nickname = findViewById(R.id.nickNameET_singUp);
+    Button duplication_nickname = findViewById(R.id.nickNameDoubleCheckBTN_singUp);
+    EditText pw = findViewById(R.id.pwET_singUp);
+    EditText pw_check = findViewById(R.id.pwCheckET_singUp);
+    EditText height = findViewById(R.id.heightET_singUp);
+    EditText weight = findViewById(R.id.weightET_singUp);
+    Spinner gender = findViewById(R.id.genderSpinner_singUp);
+    Button singup = findViewById(R.id.singUpBTN_singUp);
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_sing_up);
+
+        duplication_id.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        duplication_nickname.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        singup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
